9c75b7aa507e0f1ff609862ea5cab9b695f0f01f
JacoboGonzalezDiaz <jacobogonzalezdiaz@protonmail.com>
2026-07-15 08:00:53 +0100

    feat: add server.js for HTTP server setup with Next.js

--- tpv-restaturante/server.js
+++ tpv-restaturante/server.js
@@ -1,6 +1,6 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import { parse } from 'url';
-import next from 'next';
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = process.env.HOST || '0.0.0.0';
@@ -10,8 +10,8 @@ const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const httpServer = createServer((req: IncomingMessage, res: ServerResponse) => {
-    const parsedUrl = parse(req.url!, true);
+  const httpServer = createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   });
 
